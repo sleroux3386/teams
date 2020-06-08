28bb4e15fb73307e6d12fdc5d65fa9a63c816ea5
sleroux3386 <sleroux3386@gmail.com>
2020-06-08 16:01:38 +0100

    first commit

---
+++ precache-manifest.8ea12597676324b843ef14205ebc6c07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd252ec7598a0a6443a04ea4c25d796f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd6994c50a82a750680b",
+    "url": "/static/css/main.80168001.chunk.css"
+  },
+  {
+    "revision": "0051b715257cf3e61bb2",
+    "url": "/static/js/2.4368f9d7.chunk.js"
+  },
+  {
+    "revision": "352219eae929bbbca2636d8d92ba05e4",
+    "url": "/static/js/2.4368f9d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd6994c50a82a750680b",
+    "url": "/static/js/main.006b6db3.chunk.js"
+  },
+  {
+    "revision": "6047a219fb7954b53502",
+    "url": "/static/js/runtime-main.da4ee6aa.js"
+  }
+]);
